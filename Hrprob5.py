24f3014435b823f1ab4f56334a5c6b4d01a3651e
Srilekha hatui <hatuisrilekha@gmail.com>
2024-09-27 23:05:17 +0530

    Create Hrprob5.py

---
+++ Hrprob5.py
@@ -0,0 +1,17 @@
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+year = int(input())
+if is_leap_year(year):
+    print("True")
+else:
+    print("False")
