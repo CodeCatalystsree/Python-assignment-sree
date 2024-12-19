e3d091c91871b4315cdd8afdfe7e00a0c204ec6b
Srilekha hatui <hatuisrilekha@gmail.com>
2024-12-19 19:22:07 +0530

    Create angry_check.py
    There are two friends, John and Smith, and the parameters j_angry and s_angry to indicate if each is angry. You are in trouble if both of them are angry or no one of them is angry.
    
    Now, complete the function which returns true if you are in trouble, else return false
    
    Example 1:
    
    Input: j_angry = True, s_angry = True Output: True Explanation: Since both of them are angry, so you are in trouble.

---
+++ GFGpracques/angry_check.py
@@ -0,0 +1,8 @@
+def angryFriend(j_angry,s_angry):
+  if(j_angry==True and s_angry==True) or (j_angry==False and s_angry==False):
+    return True
+  else:
+    return False
+j_angry=input("enter -")
+s_angry=input("enter-")
+angryFriend(j_angry,s_angry)
